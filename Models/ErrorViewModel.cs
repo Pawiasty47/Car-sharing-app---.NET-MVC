7af515439fd601e5888fe7774c5259e6a019ef4f
gisztorowiczz <zuzannagisztorowicz@gmail.com>
2025-10-23 13:42:25 +0200

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projekt_zespołowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
